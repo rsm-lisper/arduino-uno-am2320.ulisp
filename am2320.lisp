64517edbd5ec88544e70d1c7c6928d935dbde3c5
Rafał -rsm- Marek <rsm@lisper.pl>
2019-02-24 03:05:11 +0100

    more readable source, some readme

--- am2320.lisp
+++ am2320.lisp
@@ -1,20 +1,23 @@
 
-;; bytes-read
+;; temp-humid-sensor-address
+(defvar tha (ash #xb8 -1))
+
+;; read-n-bytes
 ;; - n :: number of bytes to read
 ;; - str :: stream to read from
-(defun br (n str)
+(defun rnb (n str)
   (when (< 0 n)
     (cons (read-byte str)
-	  (br (1- n) str))))
+	  (rnb (1- n) str))))
 
 ;; get-temperature-humidity
 (defun gth ()
-  (with-i2c (str 92))
-  (when (with-i2c (str 92)
-	  (when str (dolist (b '(3 0 4) t)
+  (with-i2c (str tha))
+  (when (with-i2c (str tha)
+	  (when str (dolist (b '(#x03 #x00 #x04) t)
 		      (write-byte b str))))
-    (with-i2c (str 92 8)
-      (when str (let ((thd (br 8 str)))
+    (with-i2c (str tha 8)
+      (when str (let ((pck (rnb 8 str)))
 		  (list
-		   (/ (+ (ash (logand (nth 4 thd) 127) 8) (nth 5 thd)) 10)
-		   (/ (+ (ash (nth 2 thd) 8) (nth 3 thd)) 10)))))))
+		   (/ (+ (ash (logand (nth 4 pck) #b01111111) 8) (nth 5 pck)) 10)
+		   (/ (+ (ash (nth 2 pck) 8) (nth 3 pck)) 10)))))))
